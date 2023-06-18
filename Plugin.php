40fcb409b8453f083a3fa39ac9b8562ec5451457
Dreanad <louis-etienne@leuilliot.fr>
2023-06-18 17:16:57 +0200

    Update signin view on prevent_native_auth

--- Plugin.php
+++ Plugin.php
@@ -140,13 +140,25 @@ class Plugin extends PluginBase
      */
     protected function extendAuthController(): void
     {
+        // Extend the auth controller to add the SSO login buttons only if the native auth is disabled
+        if(Config::get('winter.sso::prevent_native_auth'))  {
+            Event::listen('backend.page.beforeDisplay', function ($controller, $action) {
+                if(!$controller instanceof \Backend\Classes\Controller) {
+                    return;
+                }
+                if ($action == 'signin') {
+                    $controller->addViewPath(plugins_path('winter/sso/views/auth'));
+                }
+            });
+        }
+
         // Extend the signin view to add the SSO buttons for the enabled providers
         Event::listen('backend.auth.extendSigninView', function ($controller) {
             $buttonsHtml = '';
             foreach (Config::get('winter.sso::enabled_providers', []) as $provider) {
                 $providerName = Lang::get("winter.sso::lang.providers.$provider");
                 $buttonsHtml .= View::make("winter.sso::btn.provider", [
-                    'logoUrl' => Url::asset('/plugins/winter/sso/assets/images/' . $provider . '.svg'),
+                    'logoUrl' => Url::asset('/plugins/winter/sso/assets/images/providers/' . $provider . '.svg'),
                     'logoAlt' => Lang::get('winter.sso::lang.provider_btn.alt_text', ['provider' => $providerName]),
                     'url' => Backend::url('winter/sso/handle/redirect/' . $provider),
                     'label' => Lang::get('winter.sso::lang.provider_btn.label', ['provider' => $providerName]),
