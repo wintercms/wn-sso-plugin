bd30e549faeddd4d8c012c2727bf8919f7d37639
Luke Towers <git@luketowers.ca>
2025-01-22 14:34:16 -0600

    Improve button configuration / extensibility (#10)

--- views/providers.php
+++ views/providers.php
@@ -1,14 +1,12 @@
 <?php
 
 $buttonsHtml = '';
-foreach ($providers as $provider) {
-    $providerName = Lang::get("winter.sso::lang.providers.$provider");
-
-    $buttonsHtml .= View::make("winter.sso::buttons.provider", [
-        'logoUrl' => Url::asset('/plugins/winter/sso/assets/images/providers/' . $provider . '.svg'),
-        'logoAlt' => Lang::get('winter.sso::lang.provider_btn.alt_text', ['provider' => $providerName]),
-        'url' => Backend::url('winter/sso/handle/redirect/' . $provider),
-        'label' => Lang::get('winter.sso::lang.provider_btn.label', ['provider' => $providerName]),
+foreach ($providers as $provider => $data) {
+    $buttonsHtml .= View::make("winter.sso::buttons.provider", $data['view'], [
+        'logoUrl' => $data['logoUrl'],
+        'logoAlt' => $data['logoAlt'],
+        'url' => $data['url'],
+        'label' => $data['label'],
     ]);
 } ?>
 
